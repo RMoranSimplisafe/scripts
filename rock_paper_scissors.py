023620557047203b7da749da062e9883f6f2b204
Rich Moran <richmoran@Rich-Moran.local>
2018-07-17 18:39:46 -0400

    Added the random computer turn and the incorrect input loop

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,3 +4,12 @@ from random import randint
 
 user_input = input()
 valid_moves = ['rock', 'paper', 'scissors']
+
+def computer_move():
+    return valid_moves[randint(0,2)]
+
+while user_input not in valid_moves:
+    print "Don't be dumb! Write rock, paper, or scissors."
+    user_input = input()
+    
+print computer_move()
